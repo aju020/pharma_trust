a56811314c80f575b4bc769f312d7bdca99d9772
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-05-06 22:33:19 +0530

    functionalities to update the blockchain network

--- client/src/Supply.js
+++ client/src/Supply.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useHistory } from "react-router-dom"
 import Web3 from "web3";
 import SupplyChainABI from "./artifacts/PharmaTrust.json"
+import { Box, Tab, Tabs, Typography } from '@mui/material';
 
 function Supply() {
     const history = useHistory()
@@ -16,6 +17,11 @@ function Supply() {
     const [MED, setMED] = useState();
     const [MedStage, setMedStage] = useState();
     const [ID, setID] = useState();
+    const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (event, newTabIndex) => {
+    setTabIndex(newTabIndex);
+  };
 
 
     const loadWeb3 = async () => {
@@ -68,6 +74,70 @@ function Supply() {
     const redirect_to_home = () => {
         history.push('/')
     }
+
+    const handlerChangeID = (event) => {
+        setID(event.target.value);
+    }
+    const handlerSubmitRMSsupply = async (event) => {
+        event.preventDefault();
+        try {
+            var reciept = await SupplyChain.methods.RMSsupply(ID).send({ from: currentaccount });
+            if (reciept) {
+                loadBlockchaindata();
+            }
+        }
+        catch (err) {
+            alert("An error occured!!!")
+        }
+    }
+    const handlerSubmitManufacturing = async (event) => {
+        event.preventDefault();
+        try {
+            var reciept = await SupplyChain.methods.Manufacturing(ID).send({ from: currentaccount });
+            if (reciept) {
+                loadBlockchaindata();
+            }
+        }
+        catch (err) {
+            alert("An error occured!!!")
+        }
+    }
+    const handlerSubmitDistribute = async (event) => {
+        event.preventDefault();
+        try {
+            var reciept = await SupplyChain.methods.Distribute(ID).send({ from: currentaccount });
+            if (reciept) {
+                loadBlockchaindata();
+            }
+        }
+        catch (err) {
+            alert("An error occured!!!")
+        }
+    }
+    const handlerSubmitRetail = async (event) => {
+        event.preventDefault();
+        try {
+            var reciept = await SupplyChain.methods.Retail(ID).send({ from: currentaccount });
+            if (reciept) {
+                loadBlockchaindata();
+            }
+        }
+        catch (err) {
+            alert("An error occured!!!")
+        }
+    }
+    const handlerSubmitSold = async (event) => {
+        event.preventDefault();
+        try {
+            var reciept = await SupplyChain.methods.sold(ID).send({ from: currentaccount });
+            if (reciept) {
+                loadBlockchaindata();
+            }
+        }
+        catch (err) {
+            alert("An error occured!!!")
+        }
+    }
     
     return (
         <div>
@@ -107,8 +177,79 @@ function Supply() {
                 </tbody>
             </table>
 
-            <div className="box">
-                <h1>Update</h1>
+            <div className="box-wrapper">
+                <div className="box">
+                    <h1>Update Status</h1>
+                    <Box>
+                    <Box>
+                        <Tabs value={tabIndex} onChange={handleTabChange}>
+                        <Tab label="Raw-Material Supplier" />
+                        <Tab label="Manufacturer" />
+                        <Tab label="Distributor" />
+                        <Tab label="Retailer" />
+                        </Tabs>
+                    </Box>
+                    <Box sx={{ padding: 2 }}>
+                        {tabIndex === 0 && (
+                        <Box>
+                            <h4>Enter the MEDICINE ID of the medicine for which the raw materials is to be supplied</h4>
+                            <b>Note: (Only a registered Raw Material Supplier can perform this step)</b>
+                            <form onSubmit={handlerSubmitRMSsupply}>
+                                <div className="d-flex flex-column justify-content-center align-items-center">
+                                    <input className="form-control-sm mt-3" type="text" onChange={handlerChangeID} placeholder="Enter Medicine ID" required />
+                                    <button className="btn btn-outline-success btn-sm mt-3" onSubmit={handlerSubmitRMSsupply}>Supply</button>
+                                </div>
+                            </form>
+                        </Box>
+                        )}
+                        {tabIndex === 1 && (
+                        <Box>
+                            <h4>Enter the MEDICINE ID of the medicine of which the manufacturing is done</h4>
+                            <b>Note: (Only a registered Manufacturer can perform this step)</b>
+                                <form onSubmit={handlerSubmitManufacturing}>
+                                    <div className="d-flex flex-column justify-content-center align-items-center">
+                                        <input className="form-control-sm mt-3" type="text" onChange={handlerChangeID} placeholder="Enter Medicine ID" required />
+                                        <button className="btn btn-outline-success btn-sm mt-3" onSubmit={handlerSubmitManufacturing}>Manufacture</button>
+                                    </div>
+                                </form>
+                        </Box>
+                        )}
+                        {tabIndex === 2 && (
+                        <Box>
+                            <h4>Enter the MEDICINE ID of the medicine of which the distribution is to be done after manufacturing</h4>
+                            <b>Note: (Only a registered Distributor can perform this step)</b>
+                                <form onSubmit={handlerSubmitDistribute}>
+                                    <div className="d-flex flex-column justify-content-center align-items-center">
+                                        <input className="form-control-sm mt-3" type="text" onChange={handlerChangeID} placeholder="Enter Medicine ID" required />
+                                        <button className="btn btn-outline-success btn-sm mt-3" onSubmit={handlerSubmitDistribute}>Distribute</button>
+                                    </div>
+                                </form>
+                        </Box>
+                        )}
+                        {tabIndex === 3 && (
+                        <Box>
+                            <h4>Enter the MEDICINE ID of the medicine which is kept for sale</h4>
+                            <b>Note: (Only a registered Retailer can perform this step)</b>
+                                <form onSubmit={handlerSubmitRetail}>
+                                    <div className="d-flex flex-column justify-content-center align-items-center">
+                                    <input className="form-control-sm mt-3" type="text" onChange={handlerChangeID} placeholder="Enter Medicine ID" required />
+                                    <button className="btn btn-outline-success btn-sm mt-3" onSubmit={handlerSubmitRetail}>Retail</button>
+                                </div>
+
+                                <h4>Enter the MEDICINE ID of the medicine which is sold</h4>
+                            <b>Note: (Only a registered Retailer can perform this step)</b>
+                                </form>
+                                <form onSubmit={handlerSubmitSold}>
+                                    <div className="d-flex flex-column justify-content-center align-items-center">
+                                        <input className="form-control-sm mt-3" type="text" onChange={handlerChangeID} placeholder="Enter Medicine ID" required />
+                                        <button className="btn btn-outline-success btn-sm mt-3" onSubmit={handlerSubmitSold}>Sold</button>
+                                    </div>
+                                </form>
+                        </Box>
+                        )}
+                    </Box>
+                    </Box>
+                </div>
             </div>
         </div>
     )
